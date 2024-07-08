125ad8bdaa6df430961232e42caa7bb0221a0fef
adrhill <adrian.hill@mailbox.org>
2024-07-09 00:52:47 +0200

    Minor fixes

--- img/qr/generate_qrcode.jl
+++ img/qr/generate_qrcode.jl
@@ -3,4 +3,4 @@ Pkg.activate(@__DIR__)
 using QRCoders
 
 url = "https://github.com/gdalle/JuliaCon2024-AutoDiff"
-exportqrcode(url, joinpath(@__DIR__, "qrcode.png"); width=0)
+exportqrcode(url, joinpath(@__DIR__, "qrcode.png"); width=0, pixels=660)
